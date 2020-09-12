be42ca1d5fe8ba38f92bbff0954bc8ca05dd9ea0
ATNPGO <etienne.pageau@gmail.com>
2020-09-11 21:25:58 -0400

    re-design

---
+++ precache-manifest.fd8d5a6a78d4fceb2362abd1d738e950.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d41d03006e7908d8bfe7a6049aed0ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b151627580013f96faa",
+    "url": "/static/js/2.eec496ab.chunk.js"
+  },
+  {
+    "revision": "87017439fc150bd2bfa1a9bc911f7775",
+    "url": "/static/js/2.eec496ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d87a0f1b61adb611dd2",
+    "url": "/static/js/main.14cb8c9e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
